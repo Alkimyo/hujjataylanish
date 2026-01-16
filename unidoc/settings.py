4460130d0030f9f9c0349200503e26616299be96
Tolibjon <tolibjon@kali.host>
2026-01-16 03:05:42 -0500

    Normalize CSRF trusted origins

--- unidoc/settings.py
+++ unidoc/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'documents',
+    'documents.apps.DocumentsConfig',
     'import_export',
     'widget_tweaks',
 ]
